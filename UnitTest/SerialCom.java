0eba218a833820cf1a23b34101472ac33c3adb58
BenjaminRuelle <benjamin.ruelle@2024.icam.fr>
2023-04-05 20:37:22 +0200

    modified:   UnitTest/SerialCom.java 	modified:   data.csv

--- UnitTest/SerialCom.java
+++ UnitTest/SerialCom.java
@@ -2,38 +2,44 @@ package UnitTest;
 
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 import com.fazecast.jSerialComm.SerialPort;
 
 public class SerialCom {
 
-    static public String Read(String portName){      
-        SerialPort port = SerialPort.getCommPort(portName); // replace with your port name
-        port.setBaudRate(9600);
-        port.openPort();
-        InputStream inputStream = port.getInputStream();  
-        Scanner scanner = new Scanner(inputStream);        
+    static public String Read(Scanner scanner){      
+               
         try {
             if (scanner.hasNextLine()) {
                 String input = scanner.nextLine();
-                // System.out.println(input);
+                System.out.println(input);
                 // System.out.println(input.split(";")[0]);
                 // System.out.println(input.split(";")[1]);
-                // System.out.println(input.split(";")[2]);
-                scanner.close();
+                // System.out.println(input.split(";")[2]);                
                 return input;
             }
 
         } catch (Exception e) {
             e.printStackTrace();
          }
-         scanner.close();        
+                
          return "None;None;None";
         }
     
 
     public static void main(String[] args) throws InterruptedException {
-        
-        System.out.println(Read("COM3"));        
+        SerialPort port = SerialPort.getCommPort("COM3"); // replace with your port name
+        port.setBaudRate(9600);
+        port.openPort();
+        InputStream inputStream = port.getInputStream();  
+        Scanner scanner = new Scanner(inputStream);
+
+        while(true){
+         System.out.println(Read(scanner)); 
+         TimeUnit.SECONDS.sleep(1);
+        }
+              
         
     }
 
